32138930940ef8f181a2a582e3137aefea08d2cd
tinybearc <tinybearc@gmail.com>
2016-03-11 12:03:56 +0800

    init import

---
+++ lua/frameworks/runtime-src/Classes/PluginKochavaLua.hpp
@@ -0,0 +1,23 @@
+
+#ifndef __PluginKochavaLua_h__
+#define __PluginKochavaLua_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "tolua++.h"
+#ifdef __cplusplus
+}
+#endif
+
+int register_all_PluginKochavaLua(lua_State* tolua_S);
+
+
+
+
+
+
+
+
+
+#endif // __PluginKochavaLua_h__
